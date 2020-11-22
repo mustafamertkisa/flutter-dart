19c78df2ff462df46fa895137873ba053c5164be
Mustafa Mert Kısa <56308007+mustafamertkisa@users.noreply.github.com>
2020-11-22 23:19:53 +0300

    flutter egzersizleri

---
+++ column_row_ornek/android/app/src/main/kotlin/com/mustafamert/column_row_ornek/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mustafamert.column_row_ornek
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ davul_makinesi/android/app/src/main/kotlin/com/mustafamert/davul_makinesi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mustafamert.davul_makinesi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
